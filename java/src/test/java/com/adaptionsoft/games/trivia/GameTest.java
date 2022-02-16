8353a5d0467dfcd26e51a11747d66757f1ed7758
Daniel Biniek <danbiniek@gmail.com>
2022-02-16 22:18:51 +0100

    base test

--- java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -35,4 +35,80 @@ class GameTest {
 
         assertTrue(notAWinner);
     }
+
+    @Test
+    void aPlayer_shouldNotWinaGame_whenHeAnswersIncorrectly() {
+        var game = new Game();
+        game.add("John");
+
+        game.roll(1);
+        var notAWinner = game.wrongAnswer();
+
+        assertTrue(notAWinner);
+    }
+
+    @Test
+    void aPlayer_shouldNotWinaGame_whenHeAnswersIncorrectlyMultipleTimes() {
+        var game = new Game();
+        game.add("John");
+
+        game.roll(1);
+        game.wrongAnswer();
+        game.wrongAnswer();
+        game.wrongAnswer();
+        game.wrongAnswer();
+        game.wrongAnswer();
+        var notAWinner = game.wrongAnswer();
+
+        assertTrue(notAWinner);
+    }
+
+    @Test
+    void aPlayer_shouldWinaGame_whenHeAnswersCorrectly6timesButNotInARowAndIsNotInPenalty() {
+        var game = new Game();
+        game.add("John");
+
+        game.roll(1);
+        game.wrongAnswer();
+        game.roll(1);
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.roll(1);
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.roll(1);
+        game.wasCorrectlyAnswered();
+        game.wasCorrectlyAnswered();
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.roll(1);
+        game.wrongAnswer();
+        game.roll(1);
+        var notAWinner = game.wasCorrectlyAnswered();
+
+        assertFalse(notAWinner);
+    }
+
+    @Test
+    void aPlayer_shouldWinNotaGame_whenHeAnswersCorrectly6timesButNotInARowAndIsInPenalty() {
+        var game = new Game();
+        game.add("John");
+
+        game.roll(1);
+        game.wrongAnswer();
+        game.roll(2);
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.roll(2);
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.wasCorrectlyAnswered();
+        game.wasCorrectlyAnswered();
+        game.wasCorrectlyAnswered();
+        game.wrongAnswer();
+        game.wrongAnswer();
+        var notAWinner = game.wasCorrectlyAnswered();
+
+        assertTrue(notAWinner);
+    }
 }
